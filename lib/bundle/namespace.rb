fcb7bbc12951207f920ba1a6882e9d61aa97d551
Peter H. Boling <peter.boling@gmail.com>
2025-10-03 06:56:05 -0600

    🎉 Initial Boop

---
+++ lib/bundle/namespace.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "namespace/version"
+
+module Bundle
+  module Namespace
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/bundle/namespace_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Bundle::Namespace do
+  it "has a version number" do
+    expect(Bundle::Namespace::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
